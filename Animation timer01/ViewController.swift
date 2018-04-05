34d075fc712640d4da7f404e243a7b924e9b99da
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-04-05 12:05:51 +0900

    타이머에요

--- Animation timer01/ViewController.swift
+++ Animation timer01/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var imgCounter: UILabel!
+    var counter = 1
+    var change = 1
+    var check = false
+    
+    var mytimer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        imgCounter.text = String(counter)
     }
 
 
+    @IBAction func play(_ sender: Any) {
+        mytimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(animation), userInfo: nil, repeats: true)
+    }
+    @IBAction func stop(_ sender: Any) {
+         mytimer.invalidate()
+    }
+    @objc func animation(){
+        //카운트가 5일때 1로 돌아가면서 타이머가 돌아감
+        //  if count == 5{
+        //      count = 1
+        //  } else {
+        //      count = count + 1
+        //  }
+        //카운트가1일땐 증가하다가 5를만나면 내려가면서 타이머가돌아감
+        if counter == 5 {
+            check = false
+        } else if counter == 1 {
+            check = true
+        }
+        if check == true {
+            counter = counter + 1
+        } else if check == false{
+            counter = counter - 1
+        }
+        myImageView.image = UIImage(named: "frame\(counter).png")
+        imgCounter.text = String(counter)
+    }
+    
 }
 
